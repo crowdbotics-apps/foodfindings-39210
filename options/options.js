1b6630671764fdbcd26bce79a7d2b3921396e424
Aline Tavares <aline@crowdbotics.com>
2023-04-14 10:58:05 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,7 +1,8 @@
 export const globalOptions = {
-  name: "foodfindings-39210",
-  url: "https://foodfindings-39210.botics.co",
-  api: "https://foodfindings-39210.botics.co/api/v1"
+  "name": "foodfindings-39210",
+  "url": "https://foodfindings-39210.botics.co",
+  "api": "https://foodfindings-39210.botics.co/api/v1",
+  "initialRoute": "Untitled2"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
